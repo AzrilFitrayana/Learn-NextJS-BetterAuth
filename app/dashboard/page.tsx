4fd84b2ae67a2c6f3464a97ef551bc3e37190455
azrilFitrayana <azrilfitrayana77@gmail.com>
2025-12-07 23:57:27 +0700

    create handle logout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Logout from '@/components/logout'
 
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className='flex flex-col items-center justify-center h-screen gap-4'>
+            <h1 className='text-2xl font-bold'>Dashboard</h1>
+            <Logout />
+        </div>
     )
 }
 
